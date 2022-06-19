003a82e8cacca57aaaf0e9e9397bf8655e3191cf
root <fannyli704@gmail.com>
2022-06-19 19:13:30 +0000

    added redeploy-site.sh

---
+++ redeploy-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmux kill-server 
+cd project-fl-lx
+git fetch && git reset origin/main --hard 
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt 
+tmux new -d 'flask run --host=0.0.0.0'
