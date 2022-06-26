2bcdf538dc99a2b52f86570a6595ad3363d9be15
root <fannyli704@gmail.com>
2022-06-26 01:50:46 +0000

    systemctl myportfolio

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-tmux kill-server 
 cd project-fl-lx
 git fetch && git reset origin/main --hard 
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt 
-tmux new -d 'flask run --host=0.0.0.0'
+systemctl restart myportfolio
