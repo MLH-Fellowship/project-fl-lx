b4df4d2cdc9682ca2fe18db6d20d62e226fdf05a
Fanny Li <fannyli704@gmail.com>
2022-07-10 18:10:18 -0400

    minimal mistake

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,4 +3,4 @@
 cd project-fl-lx
 git fetch && git reset origin/main --hard 
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml -d --build
+docker compose -f docker-compose.prod.yml up -d --build
